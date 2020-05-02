795c7712bdb81cd8769936f4b745e3cd778b8741
jalenng <jalen_ng@outlook.com>
2020-05-02 03:13:54 -0700

    Updated carousel cells with archive status and description

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # jalenng.github.io
 [jalenng.github.io](https://jalenng.github.io)
-My digital portfolio in website form
+My all-in-one digital portfolio, blog, and website
--- repocells.js
+++ repocells.js
@@ -4,19 +4,21 @@ request.open("GET", url, false);
 request.send();
 var response = request.response;
 var data = JSON.parse(response);
-// console.log(data);
+console.log(data);
 
 carouselInit = "<div class=\"main-carousel\" data-flickity=\'{\"pageDots\": false, \"cellAlign\": \"left\", \"dragThreshold\": 10}\'>";
 
 document.write(carouselInit);
 for (var i = 0; i < data.length; i++) {
     repoData = data[i];
+    cellClass = (repoData["archived"] ? "carousel-cell-archived" : "carousel-cell");
     url = repoData["svn_url"]
+    status = (repoData["archived"] ? "<div class=carousel-cell-archived-status>Archived</div>" : "<div class=carousel-cell-status>Pushed " + repoData["pushed_at"].substring(5, 10) + "</div>");
     header = "<h1 class=carousel-cell-header>" + repoData["name"] + "</h1>";
-    subheader = "<h4 class=carousel-cell-subheader>" + repoData["full_name"] + "</h4>";
-    info = "Last pushed on " + repoData["pushed_at"].substring(5, 10);
+    // subheader = "<h4 class=carousel-cell-subheader>" + repoData["full_name"] + "</h4>";
+    info = "<div class=carousel-cell-info>" + repoData["description"] + "</div>";
 
-     var cellCode = "<a class=\"carousel-cell\" href=\"" + url + "\">" + header + subheader + info + "</a>";
+     var cellCode = "<a class=" + cellClass + " href=\"" + url + "\">" + status + header /*+ subheader*/ + info + "</a>";
      document.write(cellCode);
 }
 
