0172c942cd74d031d3d2f218437b12352e858d18
jalenng <jalen_ng@outlook.com>
2020-06-25 13:42:57 -0700

    Updated repo cells to show language

--- repocells.js
+++ repocells.js
@@ -13,13 +13,16 @@ for (var i = 0; i < data.length; i++) {
     repoData = data[i];
     cellClass = (repoData["archived"] ? "carousel-cell-archived" : "carousel-cell");
     url = repoData["svn_url"]
-    status = (repoData["archived"] ? "<div class=carousel-cell-archived-status>Archived</div>" : "<div class=carousel-cell-status>Pushed " + repoData["pushed_at"].substring(5, 10) + "</div>");
     header = "<h1 class=carousel-cell-header>" + repoData["name"] + "</h1>";
-    // subheader = "<h4 class=carousel-cell-subheader>" + repoData["full_name"] + "</h4>";
-    info = "<div class=carousel-cell-info>" + repoData["description"] + "</div>";
 
-     var cellCode = "<a class=" + cellClass + " href=\"" + url + "\">" + status + header /*+ subheader*/ + info + "</a>";
-     document.write(cellCode);
+    pushedDate = "Pushed " + repoData["pushed_at"].substring(5, 10);
+    lang = repoData["language"];
+    status = (repoData["archived"] ? "<div class=carousel-cell-archived-status>" + lang + " • " + "Archived</div>" : "<div class=carousel-cell-status>" + lang + " • " + pushedDate + "</div>");
+
+    info = "<div class=carousel-cell-info>" + repoData["description"] + "</div>";
+    
+    var cellCode = "<a class=" + cellClass + " href=\"" + url + "\">" + header + status + info + "</a>";
+    document.write(cellCode);
 }
 
 document.write("</div>")
