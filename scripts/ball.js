d00dd314672e6029b6b40d978c368ee78b7e9d59
jalenng <jalen_ng@outlook.com>
2021-12-23 04:00:27 -0800

    Prettify js files; add alt text to index.html images

--- scripts/ball.js
+++ scripts/ball.js
@@ -1,103 +1,100 @@
-const NUM_CLICKS_FOR_ACTIVATION = 2;
+const NUM_CLICKS_FOR_ACTIVATION = 2
 
-var numClicks = 0;
-var isRunning = false;
-var fps = 60;
-var interval;
+const fps = 60
+let numClicks = 0
+let isRunning = false
+let interval
 
-var Hoffset = 0;
-var Woffset = 0;
+let Hoffset = 0
+let Woffset = 0
 
-var xPos = ball.offsetLeft;
-var yPos = ball.offsetTop;
+let xPos = ball.offsetLeft
+let yPos = ball.offsetTop
 
-var vx = 3;
-var vy = 0;
+let vx = 3
+let vy = 0
 
-var a = 9.81;
+const a = 9.81
 
-var rotation = 0;
-var angularVel = 0;
+let rotation = 0
+let angularVel = 0
 
 // Get the ball
 ball = document.getElementById('ball')
 
 // Updates the ball's position
-function updateBall() {
-    width = document.body.clientWidth;
-    height = document.body.clientHeight;
-    Hoffset = ball.offsetHeight;
-    Woffset = ball.offsetWidth;
+function updateBall () {
+  width = document.body.clientWidth
+  height = document.body.clientHeight
+  Hoffset = ball.offsetHeight
+  Woffset = ball.offsetWidth
 
-    // Update the ball style properties
-    ball.style.left = `${xPos + document.body.scrollLeft}px`;
-    ball.style.top = `${yPos + document.body.scrollTop}px`;
-    ball.style.transform = `rotate(${rotation}deg)`;
+  // Update the ball style properties
+  ball.style.left = `${xPos + document.body.scrollLeft}px`
+  ball.style.top = `${yPos + document.body.scrollTop}px`
+  ball.style.transform = `rotate(${rotation}deg)`
 
-    // Physics integration
-    xPos = xPos + vx;
-    yPos = yPos + vy;
-    vy += a / fps;
-    angularVel = vx;
-    rotation += angularVel;
+  // Physics integration
+  xPos = xPos + vx
+  yPos = yPos + vy
+  vy += a / fps
+  angularVel = vx
+  rotation += angularVel
 
-    if (yPos <= -Hoffset) {
-        stopBall();
-    }    
+  if (yPos <= -Hoffset) {
+    stopBall()
+  }
 
-    // Vertical velocity
-    if (yPos >= (height - Hoffset)) {
-        yPos = height - Hoffset;
-        if ((-vy * 0.95) < 0.25) {
-            vy = (-vy * 0.95) + 0.25;
-        }
-        else {
-            vy = 0;
-        }
+  // Vertical velocity
+  if (yPos >= (height - Hoffset)) {
+    yPos = height - Hoffset
+    if ((-vy * 0.95) < 0.25) {
+      vy = (-vy * 0.95) + 0.25
+    } else {
+      vy = 0
     }
+  }
 
-    // Horizontal velocity
-    if (xPos < 0) {
-        xPos = 0;
-        if ((-vx * 0.95) > 0.5) {
-            vx = (-vx * 0.95) - 0.5;
-        }
-        else {
-            vx = 0;
-        }
+  // Horizontal velocity
+  if (xPos < 0) {
+    xPos = 0
+    if ((-vx * 0.95) > 0.5) {
+      vx = (-vx * 0.95) - 0.5
+    } else {
+      vx = 0
     }
-    if (xPos >= (width - Woffset)) {
-        xPos = (width - Woffset);
-        if ((-vx * 0.95) < -0.5) {
-            vx = (-vx * 0.95) + 0.5;
-        }
-        else {
-            vx = 0;
-        }
+  }
+  if (xPos >= (width - Woffset)) {
+    xPos = (width - Woffset)
+    if ((-vx * 0.95) < -0.5) {
+      vx = (-vx * 0.95) + 0.5
+    } else {
+      vx = 0
     }
+  }
 }
 
 // Counts the number of clicks and starts the ball if it's been clicked enough times
-function registerBallClick() {
-    numClicks++;
-    if (numClicks == NUM_CLICKS_FOR_ACTIVATION && !isRunning) {
-        runBall();
-    }
+function registerBallClick () {
+  numClicks++
+  if (numClicks == NUM_CLICKS_FOR_ACTIVATION && !isRunning) {
+    runBall()
+  }
 }
 
 // Begins animating the ball
-function runBall() {
-    interval = setInterval(updateBall, 1 / fps);
-    document.body.style.overflow = "hidden"; // Prevents scrollbar from appearing when the ball is near the edge
-    ball.style.visibility = "visible";  // Show the ball
-    isRunning = true;
+function runBall () {
+  interval = setInterval(updateBall, 1 / fps)
+  document.body.style.overflow = 'hidden' // Prevents scrollbar from appearing when the ball is near the edge
+  ball.style.visibility = 'visible' // Show the ball
+  isRunning = true
 }
 
 // Stops animating the ball
-function stopBall() {
-    clearInterval(interval);
-    document.body.style.overflow = "scroll" // Re-enable scrolling
-    ball.style.visibility = "hidden"; // Hide the ball
-    isRunning = false;
-    numClicks = 0;
+function stopBall () {
+  clearInterval(interval)
+  document.body.style.overflow = 'scroll' // Re-enable scrolling
+  ball.style.visibility = 'hidden' // Hide the ball
+  isRunning = false
+  numClicks = 0
 }
--- scripts/repositories.js
+++ scripts/repositories.js
@@ -5,130 +5,126 @@ const MD_ICON_COMMENT_REGEX = /(?<=<!--[ ]*Icon:[ ]*)([^ ]*)(?=[ ]*-->)/g
 
 /**
  * Sends a request and returns a parsed response in JSON
- */ 
-function getResponseFromRequest(url, type='GET') {
-    const request = new XMLHttpRequest()
-    request.open(type, url, false)
-    request.send()
-    return request.response
+ */
+function getResponseFromRequest (url, type = 'GET') {
+  const request = new XMLHttpRequest()
+  request.open(type, url, false)
+  request.send()
+  return request.response
 }
 
 /**
  * Retrieves the list of public repositories for a given user
  */
-function getRepos(user) {
-    const repos = getResponseFromRequest(`https://api.github.com/users/${user}/repos?sort=pushed`)
-    const reposParsed = JSON.parse(repos);
-    return reposParsed
+function getRepos (user) {
+  const repos = getResponseFromRequest(`https://api.github.com/users/${user}/repos?sort=pushed`)
+  const reposParsed = JSON.parse(repos)
+  return reposParsed
 }
 
 /**
  * Returns the URL of the repo's icon. This URL is retrieved from the repo's README.
  */
-function getIconURL(user, repo) {  
-    // Fallback image
-    let imageURL = 'img/repos/code.png' // Fallback image
+function getIconURL (user, repo) {
+  // Fallback image
+  let imageURL = 'img/repos/code.png' // Fallback image
 
-    // Get the readme 
-    const response = getResponseFromRequest(`https://api.github.com/repos/${user}/${repo}/readme`)
-    const responseParsed = JSON.parse(response)
-    const readme = atob(responseParsed.content)
+  // Get the readme
+  const response = getResponseFromRequest(`https://api.github.com/repos/${user}/${repo}/readme`)
+  const responseParsed = JSON.parse(response)
+  const readme = atob(responseParsed.content)
 
-    // Try to match the comment with the icon comment tag in the README
-    const imageURLMatch = readme.match(MD_ICON_COMMENT_REGEX)
+  // Try to match the comment with the icon comment tag in the README
+  const imageURLMatch = readme.match(MD_ICON_COMMENT_REGEX)
 
-    console.log(imageURLMatch)
+  console.log(imageURLMatch)
 
-    if (imageURLMatch) {
-        imageURL = imageURLMatch[0]
-    }
+  if (imageURLMatch) {
+    imageURL = imageURLMatch[0]
+  }
 
-    return imageURL
+  return imageURL
 }
 
 /**
  * Populates the mainElem div with information about the repo.
  */
-function createRepoElemPromise(repo) {
+function createRepoElemPromise (repo) {
+  return new Promise((resolve, reject) => {
+    // Destructure repo object
+    const name = repo.name
+    const description = repo.description
+    const numStargazers = repo.stargazers_count
+    const language = repo.language
 
-    return new Promise((resolve, reject) => {
+    // Get image URL from first imagetag
+    const imageURL = getIconURL(GITHUB_USERNAME, name)
 
-        // Destructure repo object
-        const name = repo.name
-        const description = repo.description
-        const numStargazers = repo.stargazers_count
-        const language = repo.language
+    /** Create the child elements */
 
-        // Get image URL from first imagetag
-        let imageURL = getIconURL(GITHUB_USERNAME, name)
+    // Main
+    const mainElem = document.createElement('a')
+    mainElem.className = 'repo'
+    mainElem.href = repo.svn_url
 
-        /** Create the child elements */
+    // Left
+    const leftElem = document.createElement('div')
+    leftElem.className = 'repo-left'
 
-        // Main
-        const mainElem = document.createElement('a')
-        mainElem.className = 'repo'
-        mainElem.href = repo['svn_url']
+    // Right
+    const rightElem = document.createElement('div')
+    rightElem.className = 'repo-right'
 
-        // Left
-        const leftElem = document.createElement('div')
-        leftElem.className = 'repo-left'
+    // Image
+    const imageElem = document.createElement('img')
+    imageElem.src = imageURL
+    imageElem.className = 'repo-image'
+    imageElem.alt = `${name} image`
 
-        // Right
-        const rightElem = document.createElement('div')
-        rightElem.className = 'repo-right'
+    // Name/header
+    const nameElem = document.createElement('h1')
+    nameElem.innerText = name
 
-        // Image
-        const imageElem = document.createElement('img')
-        imageElem.src = imageURL
-        imageElem.className = 'repo-image'
-        imageElem.alt = `${name} image`
+    // Description
+    const descriptionElem = document.createElement('p')
+    descriptionElem.className = 'repo-description'
+    descriptionElem.innerText = description
 
-        // Name/header
-        const nameElem = document.createElement('h1')
-        nameElem.innerText = name
+    // Stats badge
+    const statsElem = document.createElement('div')
+    statsElem.className = 'repo-stats'
+    statsElem.innerText = `${language} • ⭐${numStargazers}`
 
-        // Description
-        const descriptionElem = document.createElement('p')
-        descriptionElem.className = 'repo-description'
-        descriptionElem.innerText = description
+    // Put it all together
+    mainElem.appendChild(leftElem)
+    mainElem.appendChild(rightElem)
 
-        // Stats badge
-        const statsElem = document.createElement('div')
-        statsElem.className = 'repo-stats'
-        statsElem.innerText = `${language} • ⭐${numStargazers}`
+    leftElem.appendChild(imageElem)
 
-        // Put it all together
-        mainElem.appendChild(leftElem)      
-        mainElem.appendChild(rightElem)
+    rightElem.appendChild(nameElem)
+    rightElem.appendChild(statsElem)
+    rightElem.appendChild(descriptionElem)
 
-        leftElem.appendChild(imageElem)
-        
-        rightElem.appendChild(nameElem)
-        rightElem.appendChild(statsElem)
-        rightElem.appendChild(descriptionElem)
-
-        resolve(mainElem);
-
-    })
-
+    resolve(mainElem)
+  })
 }
 
 /** Start of execution */
 
 // Get all repositories and filter them
 let repos = getRepos(GITHUB_USERNAME)
-repos = repos.filter(repo => !repo['archived'])
+repos = repos.filter(repo => !repo.archived)
 
 // Select repo div to populate
 const reposDiv = document.querySelector('.repositories')
 
 // Create the elements based on the repo information, and push them to the DOM
-function createElements() {
-    for (const repo of repos) {
-        createRepoElemPromise(repo).then((repoElem) => {
-            reposDiv.appendChild(repoElem)
-        })
-    }
+function createElements () {
+  for (const repo of repos) {
+    createRepoElemPromise(repo).then((repoElem) => {
+      reposDiv.appendChild(repoElem)
+    })
+  }
 }
 
 setTimeout(createElements, 0)
